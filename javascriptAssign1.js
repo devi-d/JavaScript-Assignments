9fb973d099e1f78d1ff82290f437ab4477726ae5
devi-d <99949066+devi-d@users.noreply.github.com>
2022-03-17 10:27:47 +0530

    Update javascriptAssign1.js

--- javascriptAssign1.js
+++ javascriptAssign1.js
@@ -1,20 +1,22 @@
 //Calculating area of rectangle
 class Shape {                           //Parent class
-    constructor(width,height ) {
-        this.width = width;
-        this.height = height;     
+    constructor(name,width,height) {
+        this.nameValue=name;
+        this.widthValue = width;
+        this.heightValue = height;     
     }
     getShape() {
-        return this.width,this.height;
+        return ("Name:"+this.nameValue+","+"Width:"+this.widthValue+","+"Height:"+this.heightValue)
     }
 }
 class Rectangle extends Shape {         //Rectangle inheriting Shape,.child class
-    constructor( width, height ) {
-        super(width,height );
+    constructor( name,width, height ) {
+        super(name,width,height);
     }
     getArea() {
-        return this.width * this.height;
+        return this.widthValue * this.heightValue;
     }
 }
-let rectangle = new Rectangle( 7, 8 );
-console.log( "Area of rectangle:\t\t" + rectangle.getArea() );
+let rectangle = new Rectangle( "Rectangle",7, 8 );
+console.log("Shape information is "+rectangle.getShape());
+console.log( "Area of rectangle:\t" + rectangle.getArea() );
